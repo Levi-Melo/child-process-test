a7ffa48760193adeee2e47d376367c15ac1353b3
levi <levicontat@outlook.com>
2024-10-25 09:33:55 -0300

    optimize and add envs

--- src/background-task.ts
+++ src/background-task.ts
@@ -1,6 +1,12 @@
-process.on('message', (rows:any[]) => {
-    for (const item of rows) {
-        console.log(item)
-        process.send!('item-done');
-    }
+process.on('message', (data:string[]) => {
+    let item:string[] = []
+    data.reduce((acc, input, index)=>{
+        item.push(input)
+        if(index+1 % Number(process.env.COLUMNS_SIZE) == 0){
+            acc.push(item)
+            item = []
+        }
+        return acc
+    }, [] as (string[])[])
+    process.send!('item-done');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,45 @@
 import {createReadStream} from 'fs'
-import {createInterface} from 'readline'
 import { initialize } from './cluster'
+import { Transform } from 'stream'
 
-(async ()=>{
-  const ITEMS_PER_PAGE = 1000
-  const CLUSTER_SIZE = 200
+(()=>{
+  const COLUMNS_SIZE = Number(process.env.COLUMNS_SIZE!)
+  const CLUSTER_SIZE = Number(process.env.CLUSTER_SIZE!)
+  
   const TASK_FILE = __dirname + '/background-task'
-
-    async function* getAllPagedData(itemsPerPage:number) {
-    const rl = createInterface({
-      input: createReadStream('.csv'),
-      crlfDelay: Infinity,
-    });
+  const start = Date.now()
   
-    let currentPage = [];
-    let currentPageCount = 0;
-    for await (const line of rl ) {
-    const row = line.split(',');
-    if(row.length !== 1){
-        currentPage.push(row)
-    }
-      currentPageCount++;
-  
-      if (currentPageCount >= itemsPerPage) {
-        yield currentPage;
-        currentPage = [];
-        currentPageCount = 0;
-      }
-    }
-  
-    if (currentPage.length > 0) {
-      yield currentPage;
-    }
-  }
-
-
   const cp = initialize(
-      {
-          backgroundTaskFile: TASK_FILE,
-          clusterSize: CLUSTER_SIZE,
-      }
+    {
+        backgroundTaskFile: TASK_FILE,
+        clusterSize: CLUSTER_SIZE,
+    }
   )
-  for await (const data of getAllPagedData(ITEMS_PER_PAGE)) {
-      cp.sendToChild(data)
-  }
+  let restItems:any[] = []
+  let count = 0
+  const transform = new Transform({
+    objectMode: true,
+    readableObjectMode: true,
+    writableObjectMode: true,
+    transform: (
+      data: Buffer,
+      _,
+      _callback
+    ) => {
+      count+=data.toString().split(',').length / COLUMNS_SIZE
+      const items = [...restItems, data.toString().split(',')]
+      if(items.length % COLUMNS_SIZE != 0){
+        restItems = items.slice(-items.length % COLUMNS_SIZE)
+      }
+      cp.sendToChild(items)
+      _callback();
+    },
+    flush:(_callback)=>{
+      console.log('lines ≈', count)
+      console.log(Date.now()- start)
+      _callback()
+    }
+  }).on("error", (e) => console.log(e))
+    const s = createReadStream('.csv')
+    s.pipe(transform)
 })()
